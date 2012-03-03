0d1fbae5d270a1a81c1a2db6aa74ec20a46bfdaa
Sam Deng <sdeng@pbs.org>
2012-03-03 15:14:34 -0500

    Fixed up accidentally deleted chagnes.

--- dckids/js/app.js
+++ dckids/js/app.js
@@ -62,9 +62,23 @@ jQuery(document).ready(function ($) {
 	  wax.mm.zoomer(m).appendTo(m.parent);
 	  wax.mm.interaction(m);
 	});
-
 	var e = $('#mainFlotGraph');
 	//main graphing function
 	var plot = $.plot(e, [mobileArray], options);
 
+    var mm = com.modestmaps;
+    var url = 'http://a.tiles.mapbox.com/v3/newamerica.dckids.jsonp';
+
+    wax.tilejson(url, function(tilejson) {
+        var m = new mm.Map('mainMap',
+            new wax.mm.connector(tilejson),
+            new mm.Point(700,400));
+
+        m.setCenterZoom(new mm.Location(tilejson.center[1],
+            tilejson.center[0]),
+            tilejson.center[2] - 3);
+
+        wax.mm.zoomer(m).appendTo(m.parent);
+        wax.mm.interaction(m);
+    });
 });
