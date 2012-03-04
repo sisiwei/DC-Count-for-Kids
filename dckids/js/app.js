f6e15cb5c6ad89a65d0869c4c4f5c69fb6dc1ca5
Sam Deng <sdeng@pbs.org>
2012-03-04 10:10:57 -0500

    Addeding reading/math proficiency levels.

--- dckids/js/app.js
+++ dckids/js/app.js
@@ -73,7 +73,7 @@ jQuery(document).ready(function ($) {
 										elemOrSec = $(featureItem[20]).html(),
 										mathPct = $(featureItem[22]).html(),
 										readingPct = $(featureItem[24]).html();
-									$('#school-tooltip').html('<strong>' + schoolName + '</strong><br/>Total students: '+ totalStudents +'<br /><strong>Lunches</strong><img src="http://chart.apis.google.com/chart?chs=200x120&cht=p&chco=0000FF|6633FF|6699FF|66FFFF&chds=0,700&chd=t:'+ countFreeLunch +','+ countRedPriceLunch +','+ countNonFreeOrReduced +'&chdl=Free|Reduce|Other&chma=|2&chf=bg,s,67676700" width="200" height="120" />');
+									$('#school-tooltip').html('<strong>' + schoolName + '</strong><br/>Total students: '+ totalStudents +'<br /><strong>Lunches</strong><center><img src="http://chart.apis.google.com/chart?chs=200x120&cht=p&chco=0000FF|6633FF|6699FF|66FFFF&chds=0,700&chd=t:'+ countFreeLunch +','+ countRedPriceLunch +','+ countNonFreeOrReduced +'&chdl=Free|Reduce|Other&chma=|2&chf=bg,s,67676700" width="200" height="120" /></center><br /><strong>Proficiency</strong><center><img src="http://chart.googleapis.com/chart?chxt=x,y&chxl=0:|Reading|Math&chxp=0,25,75&chs=280x240&cht=s&chd=t:25,75|'+ mathPct +','+ readingPct +'&chco=9970AB&chf=bg,s,67676700" width="280" height="240" /></center>');
 									$('#school-tooltip').css({"top": mouseY, "left": mouseX})
 									$('#school-tooltip').fadeIn(150);	
 								}
