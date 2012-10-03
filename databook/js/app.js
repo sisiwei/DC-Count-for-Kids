d162583a17455a99b2cfb580e7a36bfb35332bf7
Sisi Wei <me@sisiwei.com>
2012-10-02 21:37:26 -0400

    Another 20 pixels

--- databook/js/app.js
+++ databook/js/app.js
@@ -284,7 +284,7 @@ function scrollToFunc(crossTabNav){
 }
 
 function stickyNav(banner){
-    var bannerPos = banner.position().top + banner.height() + 65;
+    var bannerPos = banner.position().top + banner.height() + 85;
     var crossTabNav = $('.crosstab-nav-wrapper');
 
     if ($(window).scrollTop() > bannerPos){
