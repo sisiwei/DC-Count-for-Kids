36e3e5512a6394276d37844066ac612cf5f8f65c
Sisi Wei <me@sisiwei.com>
2012-10-15 21:56:46 -0400

    Test

--- databook/js/apps.js
+++ databook/js/apps.js
@@ -82,8 +82,10 @@ jQuery(document).ready(function ($) {
 	});
 
 	$.each($('#databook-content').find('.twitter').parent('a'), function(k, v){
-		$(v).attr('href', "javascript:(function(){window.twttr=window.twttr||{};var D=550,A=450,C=screen.height,B=screen.width,H=Math.round((B/2)-(D/2)),G=0,F=document,E;if(C>A){G=Math.round((C/2)-(A/2))}window.twttr.shareWin=window.open(\'http://twitter.com/intent/tweet/?url=http://www.dcactionforchildren.org/kids-count/dc-kids-count-data-tools&text=See%20how%20the%20well-being%20of%20children%20and%20families%20in%20DC%20compares%2C%20neighborhood%20by%20neighborhood.&via=ActforDChildren\',\'\',\'left=\'+H+\',top=\'+G+\',width=\'+D+\',height=\'+A+\',
-personalbar=0,toolbar=0,scrollbars=1,resizable=1\');E=F.createElement(\'script\');E.src=\'http://platform.twitter.com/widgets.js\';F.getElementsByTagName(\'head\')[0].appendChild(E)}());");
+// 		var a = "javascript:(function(){window.twttr=window.twttr||{};var D=550,A=450,C=screen.height,B=screen.width,H=Math.round((B/2)-(D/2)),G=0,F=document,E;if(C>A){G=Math.round((C/2)-(A/2))}window.twttr.shareWin=window.open('http://twitter.com/intent/tweet/?url=http://www.dcactionforchildren.org/kids-count/dc-kids-count-data-tools&text=See%20how%20the%20well-being%20of%20children%20and%20families%20in%20DC%20compares%2C%20neighborhood%20by%20neighborhood.&via=ActforDChildren','','left='+H+',top='+G+',width='+D+',height='+A+',
+// personalbar=0,toolbar=0,scrollbars=1,resizable=1');E=F.createElement('script');E.src='http://platform.twitter.com/widgets.js';F.getElementsByTagName('head')[0].appendChild(E)}());"; 
+
+		$(v).attr('href', "javascript:(function(){window.twttr=window.twttr||{};var D=550,A=450,C=screen.height,B=screen.width,H=Math.round((B/2)-(D/2)),G=0,F=document,E;if(C>A){G=Math.round((C/2)-(A/2))}window.twttr.shareWin=window.open(\'http://twitter.com/intent/tweet/?url=http://www.dcactionforchildren.org/kids-count/dc-kids-count-data-tools&text=See%20how%20the%20well-being%20of%20children%20and%20families%20in%20DC%20compares%2C%20neighborhood%20by%20neighborhood.&via=ActforDChildren\',\'\',\'left=\'+H+\',top=\'+G+\',width=\'+D+\',height=\'+A+\', personalbar=0,toolbar=0,scrollbars=1,resizable=1\');E=F.createElement(\'script\');E.src=\'http://platform.twitter.com/widgets.js\';F.getElementsByTagName(\'head\')[0].appendChild(E)}());");
 	});
 
 }); // end document ready
