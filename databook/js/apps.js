26c0d10f29c2d5882e91763be2aee79345d73711
Sisi Wei <me@sisiwei.com>
2012-10-15 21:19:36 -0400

    Adjusting rollover box.

--- databook/js/apps.js
+++ databook/js/apps.js
@@ -113,7 +113,7 @@ function buildMap(baseURL, initialMap){
         }
 
         $('.tooltip').css({
-            left: mouseX - 450,
+            left: mouseX - 255,
             top:  mouseY - 204
         });
 
