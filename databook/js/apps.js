87457f22dab31228e9d91eba933bc7fde4c43bda
Sisi Wei <me@sisiwei.com>
2012-10-15 21:20:52 -0400

    Again

--- databook/js/apps.js
+++ databook/js/apps.js
@@ -113,7 +113,7 @@ function buildMap(baseURL, initialMap){
         }
 
         $('.tooltip').css({
-            left: mouseX - 255,
+            left: mouseX - 275,
             top:  mouseY - 204
         });
 
